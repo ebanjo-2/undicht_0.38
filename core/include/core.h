e2bc5b0a5bf8b9967bc48089e765e36af70aa1f3
ebanjo-2 <eike.thed@gmail.com>
2021-02-04 18:51:55 +0100

    fixed core.h being ignored by .gitignore

---
+++ core/include/core.h
@@ -0,0 +1,9 @@
+// this file should get configured by cmake to contain the current build config
+
+// for now the config defines are hardcoded
+
+// window lib
+#define USE_GLFW
+
+// graphics lib
+#define USE_GL33
